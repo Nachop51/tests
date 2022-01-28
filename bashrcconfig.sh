3735eb4660f5429966600efd30febc14a85dbb0d
Nachop51 <nachoperalta0@gmail.com>
2022-01-28 13:53:07 -0800

    Scripts updated

---
+++ bashrcconfig.sh
@@ -0,0 +1,6 @@
+# INSERT THIS AT THE END OF THE FILE ~/.bashrc
+# Alias for git
+alias gitt="sh /git.sh"
+# Alias for fast travel trough directories
+alias hol="cd /home/holberton-system_engineering-devops"
+
