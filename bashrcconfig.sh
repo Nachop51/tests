18b53454cb35e108a6c7bd2f5a211c3887f33eb3
Nachop51 <nachoperalta0@gmail.com>
2022-03-01 15:31:36 +0000

    bashrcconfig

--- bashrcconfig.sh
+++ bashrcconfig.sh
@@ -5,4 +5,6 @@ alias gitt="sh /git.sh"
 alias hol="cd /home/holberton-system_engineering-devops"
 # Alias for ndir script
 alias ndir="sh /ndir.sh"
+# Alias to test a compiled file with the default name
+alias t="sh ./a.out"
 # Execute the bashrc with . ~/.bashrc
