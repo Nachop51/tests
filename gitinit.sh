9691dbc391501356a18c8f2aed78a5efcce8b35d
Nachop51 <nachoperalta0@gmail.com>
2022-03-01 17:46:05 +0000

    Git init script added

---
+++ gitinit.sh
@@ -0,0 +1,57 @@
+branch=main
+# Init git below here
+git init
+echo "Insert a message for the initial README:"
+read readMeMessage
+echo "$readMeMessage" >> README.md
+git add README.md
+echo "Insert a message for the first commit:"
+read commitMessage
+git commit -m "$commitMessage"
+for (( i=1; i>0; i++ ))
+do
+	echo "Custom branch? (Default is main), answer 1 for yes or 0 for default"
+	read bool
+	if (($bool == 1 || $bool == 0))
+	then
+		if (($bool == 1))
+		then
+			read $branch
+			break
+		elif (($bool == 0))
+		then
+			break
+	fi
+	else
+		echo "Insert a valid number please."
+	fi
+done
+git branch -M $branch
+echo "Insert the link for the repository:"
+read repoLink
+git remote add origin $repoLink
+for (( i=1; i>0; i++ ))
+do
+	echo "Want to save your data? (Credential helper) answer 1 for yes or 0 for no"
+	read bool
+	if (($bool == 1 || $bool == 0))
+	then
+		if (($bool == 1))
+		then
+			git config --global credential.helper store
+			break
+		elif (($bool == 0))
+		then
+			break
+	fi
+	else
+		echo "Insert a valid number please."
+	fi
+done
+echo "Insert your email:"
+read email
+git config --global user.email "$email"
+echo "Insert your username:"
+read name
+git config --global user.name "$name"
+git push -u origin $branch
--- gitvs.sh
+++ gitvs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 chmod 755 /home/holbertonschool-low_level_programming
+chmod 755 /home/holbertonschool-low_level_programming/*/
 git add *
 echo 'Enter the commit message:'
 read commitMessage
 git commit -m "$commitMessage"
 git push -u origin main
 chmod 777 /home/holbertonschool-low_level_programming
+chmod 755 /home/holbertonschool-low_level_programming/*/
