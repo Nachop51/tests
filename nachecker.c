516e909224833d8e6e2bb188f7c186f08e191959
Nachop51 <nachoperalta0@gmail.com>
2022-03-13 02:57:37 +0000

    NACHECKER ACTUALIZADO

--- nachecker.c
+++ nachecker.c
@@ -17,7 +17,8 @@ int main(void)
 	int len;
 	int len2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
+	_printf(">TODO PRINTF QUE EMPIECE POR > ES TUYO");
+	len = _printf(">Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 
 	printf("\n PRINTF TESTS \n\n");
@@ -29,22 +30,26 @@ int main(void)
 	printf("Len:[%d]\n", len2);
 	printf("Hello %i, %s\n", n, str);
 	printf("Hello %s, %s, %c\n", str, str2, a);
+	_printf(">HANDLING DE CASOS BORDES PRINTF NORMAL\n");
 	printf("%d\n", INT_MAX);
 	printf("%d\n", INT_MIN);
 	printf("%u\n", UINT_MAX);
 	printf("%%sadasd%%%s\n", str);
-	_printf("\n PRINTF RANCIO HECHO POR NACHO TESTS \n\n");
-	_printf("Length:[%d, %i]\n", len, len);
-	_printf("Negative:[%d]\n", -762534);
-	_printf("Character:[%c]\n", 'H');
-	_printf("Len:[%d]\n", len);
-	_printf("String:[%s]\n", "I am a string !");
-	_printf("Hello %i, %s\n", n, str);
-	_printf("Hello %s, %s, %c\n", str, str2, a);
-	_printf("%d\n", INT_MAX);
-	_printf("%d\n", INT_MIN);
-	_printf("%u\n", UINT_MAX);
 	_printf("%%sadasd%%%s\n", str);
+	_printf("\n>PRINTF RANCIO HECHO POR NACHO TESTS \n\n");
+	_printf(">Length:[%d, %i]\n", len, len);
+	_printf(">Negative:[%d]\n", -762534);
+	_printf(">Character:[%c]\n", 'H');
+	_printf(">Len:[%d]\n", len);
+	_printf(">String:[%s]\n", "I am a string !");
+	_printf("\n>MUCHAS VARIABLES\n\n");
+	_printf(">Hello %i, %s\n", n, str);
+	_printf(">Hello %s, %s, %c\n", str, str2, a);
+	_printf("\n>CASOS BORDES PARA INTEGERS\n\n");
+	_printf(">%d\n", INT_MAX);
+	_printf(">%d\n", INT_MIN);
+	_printf(">%u\n", UINT_MAX);
+	_printf(">%%sadasd%%%s\n", str);
 
 	char *s;
 	char *s2;
@@ -65,20 +70,25 @@ int main(void)
 
 	s4 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n";
 
-	_printf("fasdf\n %s\n %c\n %s\n %s\n %s\n %i\n\n\n", s, c, s2, s3, s4, i);
+	_printf("\n>MUCHOS CASOS JUNTOS\n");
+	_printf(">fasdf\n%% %z %s\n %c\n %s\n %s\n %s\n %i\n\n\n", s, c, s2, s3, s4, i);
 
 	unsigned int as;
 	int b;
 
 	as = -10;
+	_printf("\n>HANDLING DE CASOS RAROS\n\n");
 	b = _printf("%u\n", as);
 	_printf(">%i\n", b);
+	_printf(">PRINTF NORMAL\n");
 	b = printf("%u\n", as);
 	printf(">%i\n", b);
-	_printf("%b\n", UINT_MAX - 5); // es un numero par
-	_printf("%b\n", 52);
-	_printf("%b\n", 554);
-	_printf("%b\n", 5324);
+	_printf("\n>CASOS DE NUMEROS BINARIOS\nsi tiene > es tu printf\n");
+	_printf(">%b\n", UINT_MAX - 5); // es un numero par
+	_printf(">%b\n", 52);
+	_printf(">%b\n", 554);
+	_printf(">%b\n", 5324);
+	_printf("\n>CASOS DE NUMEROS OCTALES\nsi tiene > es tu printf\n");
 	printf("%o\n", UINT_MAX);
 	_printf(">%o\n", UINT_MAX);
 	printf("%o\n", 52);
@@ -88,9 +98,9 @@ int main(void)
 	printf("%o\n", 5324);
 	_printf(">%o\n", 5324);
 	printf("%010d\n", -10);
-	_printf("%R\n", "Hola");
-	_printf("%R\n", "Ubyn");
-	_printf("%r\n", "Hola");
+	_printf(">%R\n", "Hola");
+	_printf(">%R\n", "Ubyn");
+	_printf(">%r\n", "Hola");
 	printf("%x\n", UINT_MAX);
 	_printf("-> %x\n", UINT_MAX);
 	printf("%x\n", 160);
@@ -109,15 +119,15 @@ int main(void)
 	_printf("-> %X\n", 24908);
 	_printf("%z");
 	_printf("-> %X\n", 24908);
-	_printf("%S\n", "Best\nSchool\n");
-	_printf("sad%ztad\n");
-	len = _printf("Hello %\n", 24908);
+	_printf(">%S\n", "Best\nSchool\n");
+	_printf(">sad%ztad\n");
+	len = _printf(">Hello %\n", 24908);
 	len2 = printf("Hello %\n", 24908);	
-	_printf("%d\n", len);
+	_printf(">%d\n", len);
 	printf("%d\n", len2);
-	len = _printf("asd% wsad\n");
+	len = _printf(">asd% wsad\n");
 	len2 = printf("sad% wsad\n");
-	_printf("%d\n", len);
+	_printf(">%d\n", len);
 	printf("%d\n", len2);
 	return (0);
 }
