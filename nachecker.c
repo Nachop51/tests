62abddb19d236ab82b16a652a8d09dafee0c872f
Nachop51 <nachoperalta0@gmail.com>
2022-03-14 14:23:49 +0000

    NACHECKER 5.0

--- nachecker.c
+++ nachecker.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
+#include "../main.h"
 
 /**
  * main - Entry point
@@ -73,7 +73,8 @@ int main(void)
 	printf("%i\n", b);
 	_printf("\n>CASOS DE NUMEROS BINARIOS\nsi tiene > es tu printf\n");
 	_printf(">%b\n", UINT_MAX - 5); /* es un numero par */
-	_printf(">%b\n", 52);
+	len = _printf(">%b\n", 52);
+	_printf("%d\n", len);
 	_printf(">%b\n", 554);
 	_printf(">%b\n", 5324);
 	_printf("\n>CASOS DE NUMEROS OCTALES\nsi tiene > es tu printf\n");
@@ -90,6 +91,8 @@ int main(void)
 	_printf(">PROBANDO ROT13 Y REVERSED STRING\n\n");
 	_printf(">%R\n", "Hola");
 	_printf(">%R\n", "Ubyn");
+	/*len = _printf(">%R\n", NULL);
+	_printf(">%d\n", len - 1);*/
 	_printf(">%r\n", "Hola");
 	_printf("\n>PROBANDO HEXADECIMAL\n");
 	printf("%x\n", UINT_MAX);
@@ -111,9 +114,8 @@ int main(void)
 	_printf("-> %X\n", 24908);
 	_printf("\n>PROBANDO CASOS SIN FORMATO COMO %z\n\n");
 	_printf(">%z %w %j %\n");
-	_printf("-> %X\n", 24908);
 	_printf("\nPROBANDO %%S\n\n");
-	_printf("%S\n", "\bBest\nSchoo\tl");
+	_printf("%S\n", "\nBest\nSchoo\tl");
 	_printf("%S\n", "Be\nst\tSch\bool");
 	_printf("%S\n", "B\test\bScho\nol");
 	_printf("\n>CASOS DE FORMATEO ADENTRO DE UN STRING\n\n");
@@ -142,6 +144,8 @@ int main(void)
 	len2 = printf("%p\n", &str);
 	len = _printf(">%p\n", &str);
 	_printf(">%d, %d\n", len - 1, len2);
+	printf("%p\n", NULL);
+	_printf("%p\n", NULL);
 	len2 = printf("%z%z\n");
 	len = _printf(">%z%z\n");
 	_printf(">%d, %d\n", len - 1, len2);
